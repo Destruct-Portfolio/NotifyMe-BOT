f5350a274f4e0cfc5e562e725b5a68e06084141a
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2022-10-05 15:37:09 +0100

    mate it better

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,24 @@ import Schedule, { RecurrenceRule } from "node-schedule";
 import Locals from "./misc/locals.js";
 
 class Index {
+  private static _setup() {
+    process.on("SIGINT", function () {
+      Schedule.gracefulShutdown().then(() => process.exit(0));
+    });
+  }
 
+  private static async _task() {
+    await (await new Handler().load().check()).notify();
+  }
 
-    private static _setup (){
-
-        process.on('SIGINT', function () { 
-            Schedule.gracefulShutdown()
-            .then(() => process.exit(0))
-            })
-     
-    }
-
-    private static async _task(){
-
-        await (await new Handler()
-            .load()
-            .check())
-            .notify()
-
-    }
-
-    static async  run () {
-        console.log(`Monitoring every ${Locals.config().PeriodCheckInMinutes} minutes...`)
-        Index._setup()
-        Schedule.scheduleJob(`* */${Locals.config().PeriodCheckInMinutes} * * * *`, Index._task)
-    }
-
+  static async run() {
+    console.log(
+      `Monitoring every ${Locals.config().PeriodCheckInMinutes} minutes...`
+    );
+    //Index._setup()
+    //Schedule.scheduleJob(`* */${Locals.config().PeriodCheckInMinutes} * * * *`, Index._task)
+    await this._task();
+  }
 }
 
-await Index.run()
-
-    
+await Index.run();
