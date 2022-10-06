b49fc5eee9c0a7560f207a79fb8339d0f0f77c6a
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2022-10-06 09:56:48 +0100

    deleted index class

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,10 @@
 import Handler from "./core/handler.js";
-import Schedule, { RecurrenceRule } from "node-schedule";
+import Schedule from "node-schedule";
 import Locals from "./misc/locals.js";
 
-class Index {
-  private static _setup() {
-    process.on("SIGINT", function () {
-      Schedule.gracefulShutdown().then(() => process.exit(0));
-    });
-  }
-
-  private static async _task() {
+const job1 = Schedule.scheduleJob(
+  `* */${Locals.config().PeriodCheckInMinutes} * * * *`,
+  async function () {
     await (await new Handler().load().check()).notify();
   }
-
-  static async run() {
-    console.log(
-      `Monitoring every ${Locals.config().PeriodCheckInMinutes} minutes...`
-    );
-    //Index._setup()
-    //Schedule.scheduleJob(`* */${Locals.config().PeriodCheckInMinutes} * * * *`, Index._task)
-    await this._task();
-  }
-}
-
-await Index.run();
+);
